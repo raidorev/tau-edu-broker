1f7ac8b34eeeecc8172b85972f1805da4f49e00f
Alexander Titov <fox@raidorev.tech>
2021-12-24 12:59:04 +0600

    build: добавил gulp

---
+++ gulpfile.ts
@@ -0,0 +1,39 @@
+import autoprefixer from 'autoprefixer'
+import del from 'del'
+import gulp from 'gulp'
+import cleanCss from 'gulp-clean-css'
+import gulpIf from 'gulp-if'
+import postcss from 'gulp-postcss'
+import gulpSass from 'gulp-sass'
+import sourcemaps from 'gulp-sourcemaps'
+import sassLib from 'sass'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isProduction = !isDevelopment
+
+const sourceGlobs = {
+  styles: './resources/styles/*.s[ac]ss',
+}
+
+const sass = gulpSass(sassLib)
+
+const clean = () => del(['./dist'])
+
+const compileSass = () =>
+  gulp
+    .src(sourceGlobs.styles)
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      }).on('error', sass.logError),
+    )
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(gulpIf(isProduction, postcss([autoprefixer()])))
+    .pipe(gulpIf(isProduction, cleanCss()))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
+    .pipe(gulp.dest('./dist/css'))
+
+const build = gulp.series(clean, gulp.parallel([compileSass]))
+
+gulp.task('default', build)
+gulp.task('watch', () => gulp.watch(Object.values(sourceGlobs), build))
